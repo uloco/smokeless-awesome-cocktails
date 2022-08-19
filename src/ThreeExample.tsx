4c5f2731f4c6fae2313c8c47fcefbc7ae3c1389c
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-08-19 10:39:35 +0200

    Add main bg

---
+++ src/ThreeExample.tsx
@@ -0,0 +1,43 @@
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<Mesh>(null!);
+
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export default function ThreeExample() {
+  return (
+    <Canvas>
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <ambientLight intensity={0.7} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[0, 0, 0]} />
+      <Box position={[-1, 1, -5]} />
+      <Box position={[0.5, 0.5, 2]} />
+    </Canvas>
+  );
+}
