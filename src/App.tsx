bbad752b94f480800bd0afc2fe346ef95fd00c05
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-08-19 11:40:43 +0200

    Add Ingredient component

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,32 @@ const Button = styled.button`
   color: #890200;
 `;
 
+const Ingredient = styled.div`
+  padding-right: 40px;
+  color: white;
+  font-family: "Barlow Condensed";
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 0.88px;
+  line-height: 18px;
+  border-bottom: 1px solid white;
+  flex-direction: row;
+  max-width: 300px;
+`;
+
 export const App = () => {
   return (
     <>
       <HomeScreen />
       <Button>Go</Button>
+      <Ingredient>IIngredientIngredientngredient</Ingredient>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Ingredient>IIngredientIngredientngredient</Ingredient>
+        <div style={{ width: 10 }}></div>
+        <Ingredient>Ingredient</Ingredient>
+        <div style={{ width: 10 }}></div>
+        <Ingredient>Ingredient</Ingredient>
+      </div>
     </>
   );
 };
