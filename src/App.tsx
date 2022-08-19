1c20439bc2ae99a9bf56019d1489f70dd0b59f2f
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-08-19 11:23:03 +0200

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,30 @@ const HomeScreen = styled.div`
   margin: 0 auto;
 `;
 
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  min-width: 200px;
+  background-color: white;
+  border-style: none;
+  border-radius: 25px;
+  font-family: "Barlow Condensed";
+  font-size: 16px;
+  letter-spacing: 1.01px;
+  line-height: 19px;
+  font-weight: 800;
+  text-transform: uppercase;
+  color: #890200;
+`;
+
 export const App = () => {
-  return <HomeScreen />;
+  return (
+    <>
+      <HomeScreen />
+      <Button>Go</Button>
+    </>
+  );
 };
